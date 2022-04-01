475732bac9ca910c9e7c0b62d30dcb826edb2e9b
Pedro Ramos <pedroramos327698@gmail.com>
2022-04-01 12:02:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const name = 'Pedro';
+
+const newname = 'newname'
+const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
+      <h1>Olá {name}</h1>
       <p>Meu primeiro app</p>
+      <img src={url} alt='minha img'/>
     </div>
   );
 }
