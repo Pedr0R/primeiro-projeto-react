a6ee895fc48266f9a88330f25d3ed1e3e9270121
Pedro Ramos <pedroramos327698@gmail.com>
2022-04-04 13:35:57 -0300

    aulas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 const url = 'https://via.placeholder.com/200'
 
   return (
     <div className="App">
+      <Frase />
+      <List />
+      <Frase />
+      <h1>Testando CSS</h1>
       <img src={url} alt='minha img '/>
-      <SayMyName nome="Pedro"></SayMyName>
+      <SayMyName nome="Pedro" />
       <Pessoa nome="André" idade="32" profissão="Recruiter" foto="https://via.placeholder.com/200"/>
     </div>
   );
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+function List() {
+  return(
+  <>
+    <h1>My list</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+    </ul>
+  </>
+  )
+}
+
+export default List
